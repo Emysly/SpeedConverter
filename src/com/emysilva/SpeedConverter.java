210553a2dcc02d03d4f672f8e025562b43b1680a
emysly <emysilva@users-MacBook-Pro.local>
2020-07-23 22:54:25 +0400

    speed converter implementation

--- src/com/emysilva/SpeedConverter.java
+++ src/com/emysilva/SpeedConverter.java
@@ -1,5 +1,15 @@
 package com.emysilva;
 
 public class SpeedConverter {
-
+    public static void main(String[] args) {
+        System.out.println(toMilesPerHour(-2000));
+    }
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            long milePerHour = Math.round(kilometersPerHour * 0.6213711922);
+            return milePerHour;
+        }
+    }
 }
